c8e64047b36a4865d39acddc4839c16c6611d4e1
David McGuffey <dmcguffey94@gmail.com>
2023-07-03 18:59:31 -0500

    exercise complete with log for html

--- app.js
+++ app.js
@@ -4,7 +4,37 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "David";
+let lastName;
+var age;
+
 
 // Exercise 2
+lastName = "McGuffey";
+age = 29;
+console.log(firstName + " " + lastName);
+console.log(age);
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+console.log(language + "" + createdYear);
+
+// Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+console.log(price + "" + salePercentage + "" + stock + selectedSize);
+
+// Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasread = true;
+console.log("The author of this book is " + author + ", and the book is called " + title
+    + ". I am currently on page " + bookmark + " out of " + pageCount + ".");
